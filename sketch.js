3b7ba85f5dad85da0250545fce87310dbd51289d
Enrico <enrico.isidori@gmail.com>
2022-12-08 10:57:10 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,21 @@
 function preload() {
-	// put preload code here
+  // put preload code here
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
-	console.log("gruppo10")
+  createCanvas(windowWidth, windowHeight);
+  // put setup code here
+  const message =
+    "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
+  textAlign(CENTER, CENTER);
+  textSize(16);
+
+  //dbsjskds
+
+  text(message, width / 2, height / 2);
+  console.log("gruppo10");
 }
 
 function draw() {
-	// put drawing code here
+  // put drawing code here
 }
