e0bf7b6db366d541ff2633bc3611289e0a2a8d41
francescobonetti <84150908+francescobonetti@users.noreply.github.com>
2022-12-08 11:51:05 +0100

    Abbiamo creato un server

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("up and running")
+let express = require("express");
+let app = express();
+let port = 3000;
+let server = app.listen(port);
+
+console.log("running server on http://localhost:" + port);
+
+app.use(express.static("public"))
