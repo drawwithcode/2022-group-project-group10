11db4fd6f2a0103f264d941db784880b5613b2f1
Marte0 <matteodellagostino@gmail.com>
2023-01-13 15:14:39 +0100

    scanner colori funzionante

--- public/SketchScanner.js
+++ public/SketchScanner.js
@@ -1,105 +1,116 @@
-
 // https://kylemcdonald.github.io/cv-examples/
 
 var capture;
-var w = 1080;
-var h = 1920;
+var w = 100;
+var h = 100;
 
 function setup() {
-    capture = createCapture({
-        audio: false,
-        video: {
-            facingMode: 
-              {
-                exact: "environment"
-              },
-            width: w,
-            height: h,
-        }
-    }, function() {
-        console.log('capture ready.')
-    });
-    capture.elt.setAttribute('playsinline', '');
-    capture.size(w, h);
-    createCanvas(w, h);
-    capture.hide();
+  capture = createCapture(
+    {
+      audio: false,
+      video: {
+        facingMode: {
+          exact: "user",
+        },
+        width: w,
+        height: h,
+      },
+    },
+    function () {
+      console.log("capture ready.");
+    }
+  );
+  capture.elt.setAttribute("playsinline", "");
+  capture.size(w, h);
+  createCanvas(w, h);
+  capture.hide();
 }
 
 var trailPointsLength = 100;
 var trailPoints = [];
 function drawTrail(nextPoint) {
-    trailPoints.push(nextPoint);
-    if (trailPoints.length > trailPointsLength) {
-        trailPoints.shift();
-    }
-    beginShape();
-    trailPoints.forEach(function (point) {
-        vertex(point.x, point.y);
-    })
-    endShape();
+  trailPoints.push(nextPoint);
+  if (trailPoints.length > trailPointsLength) {
+    trailPoints.shift();
+  }
+  beginShape();
+  trailPoints.forEach(function (point) {
+    vertex(point.x, point.y);
+  });
+  endShape();
 }
 
-var targetColor = [236, 85, 85];
+//var targetColor = [236, 85, 85];
+var targetColors = [
+  // { name: "green", r: 18, g: 238, b: 95, diff: 0, amount: 0 },
+  // { name: "purple", r: 255, g: 171, b: 223, diff: 0, amount: 0 },
+  // { name: "yellow", r: 76, g: 172, b: 236, diff: 0, amount: 0 },
+  // { name: "red", r: 85, g: 85, b: 236, diff: 0, amount: 0 },
+
+  { name: "r", r: 255, g: 0, b: 0, diff: 0, amount: 0 },
+  { name: "g", r: 0, g: 255, b: 0, diff: 0, amount: 0 },
+  { name: "b", r: 0, g: 0, b: 255, diff: 0, amount: 0 },
+];
+
 function draw() {
-    capture.loadPixels();
-    var sampling = false;
-    var sumPosition = createVector(0, 0);
-    if (capture.pixels.length > 0) { // don't forget this!
+  capture.loadPixels();
+  var sampling = false;
 
-        /* if (mouseIsPressed &&
-            mouseX > 0 && mouseX < width &&
-            mouseY > 0 && mouseY < height) {
-            targetColor = capture.get(mouseX, mouseY);
-            sampling = true;
-        } */
+  if (capture.pixels.length > 0) {
+    var w = capture.width,
+      h = capture.height;
+    var i = 0;
+    var pixels = capture.pixels;
+    var thresholdAmount = select("#thresholdAmount").value();
+    thresholdAmount /= 100; // this is the slider range
+    thresholdAmount *= 255 * 3; // this is the maximum value
+    // var total = 0;
+    for (var y = 0; y < h; y++) {
+      for (var x = 0; x < w; x++) {
+        targetColors.forEach((color) => {
+          color.diff = Math.abs(pixels[i + 0] - color.r) + Math.abs(pixels[i + 1] - color.g) + Math.abs(pixels[i + 2] - color.b);
+        });
 
-        var w = capture.width,
-            h = capture.height;
-        var i = 0;
-        var pixels = capture.pixels;
-        var thresholdAmount = select('#thresholdAmount').value();
-        thresholdAmount /= 100.; // this is the slider range
-        thresholdAmount *= 255 * 3; // this is the maximum value
-        var total = 0;
-        for (var y = 0; y < h; y++) {
-            for (var x = 0; x < w; x++) {
-                var diff =
-                    Math.abs(pixels[i + 0] - targetColor[0]) +
-                    Math.abs(pixels[i + 1] - targetColor[1]) +
-                    Math.abs(pixels[i + 2] - targetColor[2]);
-                var outputValue = 0;
-                if (diff < thresholdAmount) {
-                    outputValue = 255;
-                    sumPosition.x += x;
-                    sumPosition.y += y;
-                    total++;
-                }
-                pixels[i++] = outputValue; // set red
-                pixels[i++] = outputValue; // set green
-                pixels[i++] = outputValue; // set blue
-                i++; // skip alpha
+        var smallestDiff = Math.min(targetColors[0].diff, targetColors[1].diff, targetColors[2].diff);
+
+        targetColors.forEach((color) => {
+          if (color.diff == smallestDiff) {
+            if (color.diff < thresholdAmount) {
+              color.amount++;
             }
-        }
+          }
+        });
 
-        sumPosition.div(total);
+        //var outputValue = 0;
 
-        var n = w * h;
-        var ratio = total / n;
-        select('#percentWhite').elt.innerText = int(100 * ratio);
-    }
-    if (!sampling) {
-        capture.updatePixels();
+        i = i + 4; // skip alpha
+      }
     }
 
-    image(capture, 0, 0, w, h);
+    // var n = w * h;
+    // var ratio = total / n;
+    // select("#percentWhite").elt.innerText = int(100 * ratio);
+  }
+  if (!sampling) {
+    capture.updatePixels();
+  }
 
-    noStroke();
-    fill(targetColor);
-    rect(20, 20, 40, 40);
+  image(capture, 0, 0, w, h);
 
-    ellipse(sumPosition.x, sumPosition.y, 8, 8);
-    noFill();
-    stroke(targetColor);
-    strokeWeight(8);
-    drawTrail(sumPosition);
+  let temp = 0;
+
+  //console.log("color amount:");
+  targetColors.forEach((color) => {
+    if (color.amount > temp) {
+      temp = color.amount;
+    }
+    //console.log(color.name + ":  " + color.amount);
+  });
+
+  targetColors.forEach((color) => {
+    if (color.amount == temp) {
+      if (color.amount > 7000) console.log(color.name + "  :  " + color.amount);
+    }
+    color.amount = 0;
+  });
 }
--- public/client_sketch.js
+++ public/client_sketch.js
@@ -9,29 +9,29 @@ let osc, playing, amp;
 let messageForm = document.getElementById("send-container");
 let messageInput = document.getElementById("message-input");
 let sendButton = document.getElementById("send-button");
-let chat = document.querySelector(".messages-container")
+let chat = document.querySelector(".messages-container");
 
 //Navigazione automatica che attiva e disattiva le sezioni
-let navBtn = document.querySelectorAll(".navbtn")
-let currentActiveBtn = document.querySelector(".navbtn.active")
-let sections = document.querySelectorAll(".container")
-let currentSection = document.querySelector(".container.active")
+let navBtn = document.querySelectorAll(".navbtn");
+let currentActiveBtn = document.querySelector(".navbtn.active");
+let sections = document.querySelectorAll(".container");
+let currentSection = document.querySelector(".container.active");
 
-navBtn.forEach( function(btn) {
-  btn.addEventListener("click", function() {
-  let id = btn.id
-  let sectionClass = String("." + id + "-container")
-  let targetSection = document.querySelector(sectionClass)
-  
-  currentActiveBtn.classList.remove("active")
-  btn.classList.add("active")
-  currentActiveBtn = btn
-  
-  currentSection.classList.remove("active")
-  targetSection.classList.add("active")
-  currentSection = targetSection;
-  
-})})
+navBtn.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    let id = btn.id;
+    let sectionClass = String("." + id + "-container");
+    let targetSection = document.querySelector(sectionClass);
+
+    currentActiveBtn.classList.remove("active");
+    btn.classList.add("active");
+    currentActiveBtn = btn;
+
+    currentSection.classList.remove("active");
+    targetSection.classList.add("active");
+    currentSection = targetSection;
+  });
+});
 
 //oggetto utente che contiene ruolo, index, colore e messaggio che vuole mandare
 //inizialmente vuoto, poi viene definito con updateUsers
@@ -40,55 +40,57 @@ const user = {
   index: "",
   c: "",
   message: "",
-  freq: ""
-}
+  freq: "",
+};
 
 clientSocket.on("connect", newConnection);
 clientSocket.on("updateUsers", updateUsers);
 
 clientSocket.on("message-request", sendMessage);
 clientSocket.on("broadcast-message", saveMessage);
-clientSocket.on("show-message",showMessage);
+clientSocket.on("show-message", showMessage);
 
-sendButton.addEventListener("click", function() {
+sendButton.addEventListener("click", function () {
   user.message = messageInput.value;
-  messageInput.value = ""
-  console.log(user.message)
+  messageInput.value = "";
+  console.log(user.message);
   osc.start();
-})
-
+});
 
 //avvisa il server di essere entrato, in modo che possa aggiornare lo userArray e rimandarlo indietro
 function newConnection() {
-  clientSocket.emit("enter-room")
+  clientSocket.emit("enter-room");
 }
 
 //assegnazione ruolo client o server
 function updateUsers(userArray) {
-  
-  console.log(userArray)
+  console.log(userArray);
 
   user.index = userArray.indexOf(clientSocket.id);
-  if (user.index ==0) {window.location.href = "serverino_index.html";} //se uno entra dalla pagina del client quando il posto è libero, diventa server
-  if (user.index > 4) {window.location.href = "index.html";}
-  user.c = colorsArray[user.index]
-  user.freq = frequencyArray[user.index]
+  if (user.index == 0) {
+    window.location.href = "serverino_index.html";
+  } //se uno entra dalla pagina del client quando il posto è libero, diventa server
+  if (user.index > 4) {
+    window.location.href = "index.html";
+  }
+  user.c = colorsArray[user.index];
+  user.freq = frequencyArray[user.index];
   osc = new p5.Oscillator("sawtooth");
   osc.freq(user.freq, 0.1);
-  console.log(user.freq)
-  user.role = "client"
+  console.log(user.freq);
+  user.role = "client";
 }
 
 function sendMessage() {
-  if (typeof user.message != "undefined" && user.message != ""){
+  if (typeof user.message != "undefined" && user.message != "") {
     console.log("sto inviando:  " + user.message);
     clientSocket.emit("send-chat-message", user);
 
-    let div = createDiv(user.message)
-    div.parent(chat)
-    let divClass = "client" + user.index
-    div.addClass("box")
-    div.addClass(divClass)
+    let div = createDiv(user.message);
+    div.parent(chat);
+    let divClass = "client" + user.index;
+    div.addClass("box");
+    div.addClass(divClass);
   }
 }
 
@@ -97,20 +99,21 @@ function saveMessage(user) {
   recievedMessageIndex = user.index;
 }
 
-function showMessage(){
-  let div = createDiv(recievedMessage)
-  div.parent(chat)
-  let divClass = "client" + recievedMessageIndex
-  div.addClass("box")
-  div.addClass(divClass)
+function showMessage() {
+  let div = createDiv(recievedMessage);
+  div.parent(chat);
+  let divClass = "client" + recievedMessageIndex;
+  div.addClass("box");
+  div.addClass(divClass);
 }
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  colorsArray = [color('#e6e6e6'), color('#5fee87'), color('#dfabff'), color('#ecac4c'), color('#ec5555')];
+  colorsArray = [color("#e6e6e6"), color("#5fee87"), color("#dfabff"), color("#ecac4c"), color("#ec5555")];
 }
 
 function draw() {
-  if(user.c) {background(user.c);}
+  if (user.c) {
+    background(user.c);
+  }
 }
