12a1851c76f112edb2549718815d05086ac610f8
francescobonetti <84150908+francescobonetti@users.noreply.github.com>
2023-01-17 15:35:08 +0100

    camera nel collect

--- public/client_sketch.js
+++ public/client_sketch.js
@@ -1,6 +1,8 @@
 let clientSocket = io();
 let colorsArray;
 let frequencyArray = [100, 300, 500, 700, 900];
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight
 
 let recievedMessage;
 let recievedMessageIndex;
@@ -73,11 +75,6 @@ function updateUsers(userArray) {
   if (user.index > 4) {
     window.location.href = "index.html";
   }
-  user.c = colorsArray[user.index];
-  user.freq = frequencyArray[user.index];
-  osc = new p5.Oscillator("sawtooth");
-  osc.freq(user.freq, 0.1);
-  console.log(user.freq);
   user.role = "client";
 }
 
@@ -85,7 +82,6 @@ function sendMessage() {
   if (typeof user.message != "undefined" && user.message != "") {
     console.log("sto inviando:  " + user.message);
     clientSocket.emit("send-chat-message", user);
-
     let div = createDiv(user.message);
     div.parent(chat);
     let divClass = "client" + user.index;
@@ -107,13 +103,244 @@ function showMessage() {
   div.addClass(divClass);
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  colorsArray = [color("#e6e6e6"), color("#5fee87"), color("#dfabff"), color("#ecac4c"), color("#ec5555")];
+
+let sketch = function(p) {
+    
 }
 
-function draw() {
+let p1 = new p5(sketch);
+
+p1.setup = function() {
+  p1.createCanvas(windowWidth, windowHeight);
+  colorsArray = [p1.color("#e6e6e6"), p1.color("#5fee87"), p1.color("#dfabff"), p1.color("#ecac4c"), p1.color("#ec5555")];
+}
+
+p1.draw = function() {
+  user.c = colorsArray[user.index];
   if (user.c) {
-    background(user.c);
+    p1.background(user.c);
   }
 }
+
+//********************************************************  SCANNER COLORI ********************************************************  //
+
+let p2 = new p5(sketch);
+
+var capture;
+var w = window.innerWidth;
+var h = w;
+let subW = Math.round(w / 3)
+let subH = Math.round(w / 3/9*16)
+let subX = Math.round(w / 2 - subW / 2)
+let subY = Math.round(h / 2 - subH / 2)
+
+let ctotal = 0;
+let threshold = 15; 
+let targetColors;
+let targetPercent = 0.7;
+
+let avgHueArray = [];
+let avgSatArray = [];
+let avgLigArray = [];
+
+let hue;
+let saturation;
+let lightness;
+
+let collectContainer = document.querySelector(".collect-container")
+
+p2.setup = function() {
+  let collectCanva = p2.createCanvas(windowWidth, windowHeight);
+  collectCanva.parent(collectContainer)
+  let d = p2.pixelDensity()
+  p2.colorMode(p2.HSL, 360, 100, 100)
+  p2.pixelDensity(1);
+
+  targetColors = [{name: "green", hue: 137, saturation: 81, lightness: 65, total: 0}, {name: "purple", hue: 277, saturation: 100, lightness: 84, total: 0}, {name: "yellow", hue: 36, saturation: 81, lightness: 61, total: 0}, {name: "red", hue: 360, saturation: 80, lightness: 63, total: 0}]
+  
+  capture = p2.createCapture({
+      audio: false,
+      video: {
+          facingMode: {
+            exact: "environment",
+          },
+          width: w,
+          height: h
+      }
+  }, function() {
+      console.log('capture ready')
+  });
+  capture.elt.setAttribute('playsinline', '');
+  capture.size(w, h);
+  capture.hide();
+  p2.frameRate(20)
+
+}
+
+p2.draw = function() {
+  user.c = colorsArray[user.index];
+  if (user.c) {
+    p2.background(user.c);
+  }
+
+  targetColors.forEach(function(color) {color.total = 0}) //resets total
+
+  p2.image(capture, 0, 0)
+  p2.noFill()
+  p2.rect(subX-1, subY-1, subW+2, subH+2)
+
+  avgHueArray = [];
+  avgSatArray = [];
+  avgLigArray = [];
+  let colorFound = "nessun colore trovato"
+
+  p2.loadPixels()
+
+  for (let x = subX; x < subX + subW; x++) {
+    for (let y = subY; y < subY + subH; y++){
+      
+      let loc = (x + y * capture.width) * 4;
+    
+      let r = p2.pixels[loc + 0]
+      let g = p2.pixels[loc + 1]
+      let b = p2.pixels[loc + 2]
+
+      
+      hue = 0;
+      saturation = 0;
+      lightness = 0;
+
+      RGBToHSL(r, g, b)
+
+      avgHueArray.push(hue)
+      avgSatArray.push(saturation)
+      avgLigArray.push(lightness)
+      
+      //per trovare colori specifici 
+        
+      targetColors.forEach(function (color)
+      {
+        let hueDiff = Math.abs(hue - color.hue)
+        let satDiff = Math.abs(saturation - color.saturation)
+        let briDiff = Math.abs(lightness - color.lightness)
+
+        if(hue < 20 && satDiff < 25 && briDiff < 25) {
+          targetColors[3].total ++
+        }
+        else if (hueDiff < 30 && satDiff < 25 && briDiff < 25) {
+          color.total ++ 
+        }
+      }) 
+      
+    }
+  }
+
+  //per tracking sotto di h s b
+  findAverageColor() 
+
+  targetColors.forEach(function(color) {
+    let ratio = 100 * color.total / (subW * subH)
+    if (ratio >= 70) {colorFound = "HO TROVATO: " + color.name}
+  })
+  
+  p2.textAlign(p2.CENTER)
+  p2.textSize(32)
+  p2.fill("white")
+  p2.text(colorFound, p2.width/2, 50)
+
+  
+}
+
+function RGBToHSL(r, g, b) {
+  // Make r, g, and b fractions of 1
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  // Find greatest and smallest channel values
+  let cmin = Math.min(r, g, b),
+      cmax = Math.max(r, g, b),
+      delta = cmax - cmin
+  
+  // Calculate hue
+  // No difference
+  if (delta == 0)
+    hue = 0;
+  // Red is max
+  else if (cmax == r)
+    hue = ((g - b) / delta) % 6;
+  // Green is max
+  else if (cmax == g)
+    hue = (b - r) / delta + 2;
+  // Blue is max
+  else
+    hue = (r - g) / delta + 4;
+
+  hue = Math.round(hue * 60);
+    
+  // Make negative hues positive behind 360°
+  if (hue < 0)
+      hue += 360;
+
+  // Calculate lightness
+  lightness = (cmax + cmin) / 2;
+
+  // Calculate saturation
+  saturation = delta == 0 ? 0 : delta / (1 - Math.abs(2 * lightness - 1));
+    
+  // Multiply l and s by 100
+  saturation = +(saturation * 100).toFixed(1);
+  lightness = +(lightness * 100).toFixed(1);
+  
+}
+
+
+function findAverageColor() {
+  
+  let totalHue = 0;
+  let totalSat = 0;
+  let totalLig = 0;
+
+
+  for(let i = 0; i < avgHueArray.length; i++) {
+
+    totalHue += avgHueArray[i]
+    totalSat += avgSatArray[i]
+    totalLig += avgLigArray[i]
+
+  }
+
+  let avgHue = totalHue / avgHueArray.length
+  let avgSat = totalSat / avgSatArray.length
+  let avgLig = totalLig / avgLigArray.length
+  
+
+  p2.textAlign(p2.LEFT)
+  p2.textSize(16)
+
+  p2.fill("black")
+  p2.text("hue", 10, w+30)
+  p2.text("saturation", 10, w+130)
+  p2.text("brightness", 10, w+230)
+
+  p2.colorMode(p2.HSL, 360, 100, 100)
+  p2.fill(avgHue, avgSat, avgLig)
+  p2.rect(10, w + 50, p2.map(avgHue, 0, 360, 0, p2.width-50), 30)
+  p2.rect(10, w + 150, p2.map(avgSat, 0, 100, 0, p2.width-50), 30)
+  p2.rect(10, w + 250, p2.map(avgLig, 0, 100, 0, p2.width-50), 30)
+  //p2.rect(subX-1, subY-1, subW+2, subH+2)
+
+  p2.fill("black")
+  p2.text(p2.round(avgHue), p2.map(avgHue, 0, 360, 0, p2.width-50) + 20, w+50+15)
+  p2.text(p2.round(avgSat), p2.map(avgSat, 0, 100, 0, p2.width-50) + 20, w+150+15)
+  p2.text(p2.round(avgLig), p2.map(avgLig, 0, 100, 0, p2.width-50) + 20, w+250+15)
+}
+
+
+
+
+
+
+
+
+ 
